89ab7f348a1de9f882e980cc246c36f1d8c304cc
Carlsen66 <37250123+Carlsen66@users.noreply.github.com>
2018-04-04 21:14:17 +0200

    Doodle version with progressbar
    Add simple progressbar
    Add guess after drawing a line.

--- examples/doodle_classification/sketch.js
+++ examples/doodle_classification/sketch.js
@@ -15,6 +15,12 @@ let rainbows = {};
 
 let nn;
 
+let train = false;
+let counter = 0;
+
+let training = [];
+let testing = [];
+
 function preload() {
   catsData = loadBytes('data/cats1000.bin');
   trainsData = loadBytes('data/trains1000.bin');
@@ -23,6 +29,7 @@ function preload() {
 
 
 function setup() {
+  frameRate(60);
   createCanvas(280, 280);
   background(255);
 
@@ -35,58 +42,44 @@ function setup() {
   nn = new NeuralNetwork(784, 64, 3);
 
   // Randomizing the data
-  let training = [];
+  //let training = [];
   training = training.concat(cats.training);
   training = training.concat(rainbows.training);
   training = training.concat(trains.training);
 
-  let testing = [];
+  //let testing = [];
   testing = testing.concat(cats.testing);
   testing = testing.concat(rainbows.testing);
   testing = testing.concat(trains.testing);
 
   let trainButton = select('#train');
   let epochCounter = 0;
-  trainButton.mousePressed(function() {
-    trainEpoch(training);
+  trainButton.mousePressed(function () {
+    //trainEpoch(training);
+    document.getElementById("infotext").innerHTML = "Traning";
+    train = true;
+    background(255);
+    shuffle(training, true);
     epochCounter++;
     console.log("Epoch: " + epochCounter);
   });
 
   let testButton = select('#test');
-  testButton.mousePressed(function() {
+  testButton.mousePressed(function () {
+    document.getElementById("infotext").innerHTML = "Traning";
     let percent = testAll(testing);
+    document.getElementById("infotext").innerHTML = "Percent: " + nf(percent, 2, 2) + "%";
     console.log("Percent: " + nf(percent, 2, 2) + "%");
   });
 
   let guessButton = select('#guess');
-  guessButton.mousePressed(function() {
-    let inputs = [];
-    let img = get();
-    img.resize(28, 28);
-    img.loadPixels();
-    for (let i = 0; i < len; i++) {
-      let bright = img.pixels[i * 4];
-      inputs[i] = (255 - bright) / 255.0;
-    }
+  guessButton.mousePressed(Guess);
 
-    let guess = nn.predict(inputs);
-    // console.log(guess);
-    let m = max(guess);
-    let classification = guess.indexOf(m);
-    if (classification === CAT) {
-      console.log("cat");
-    } else if (classification === RAINBOW) {
-      console.log("rainbow");
-    } else if (classification === TRAIN) {
-      console.log("train");
-    }
 
-    //image(img, 0, 0);
-  });
 
   let clearButton = select('#clear');
-  clearButton.mousePressed(function() {
+  clearButton.mousePressed(function () {
+    document.getElementById("infotext").innerHTML = "Doodle";
     background(255);
   });
   // for (let i = 1; i < 6; i++) {
@@ -97,11 +90,70 @@ function setup() {
   // }
 }
 
+let mousepressed = false;
 
 function draw() {
   strokeWeight(8);
   stroke(0);
   if (mouseIsPressed) {
-    line(pmouseX, pmouseY, mouseX, mouseY);
+    //console.log(mouseY +"<="+ canvas.height +"&&"+ mouseX +"<="+ canvas.width)
+    if (mouseY <= canvas.height && mouseX <= canvas.width) {
+      mousepressed = true;
+      line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+  } else if (mousepressed) {
+    mousepressed = false;
+    Guess();
+  }
+  if (train) {
+    if (counter < training.length) {
+      counter = trainEpoch(training, counter);
+    } else {
+      counter = 0;
+      train = false;
+      console.log("finnish training");
+      document.getElementById("infotext").innerHTML = "Finnish";
+      let percent = testAll(testing);
+      document.getElementById("iqtext").innerHTML = nf(percent, 2, 2) + "%";
+    }
   }
 }
+
+function Guess() {
+  let inputs = [];
+  let img = get();
+  let infotext = document.getElementById("infotext");
+  img.resize(28, 28);
+  img.loadPixels();
+  for (let i = 0; i < len; i++) {
+    let bright = img.pixels[i * 4];
+    inputs[i] = (255 - bright) / 255.0;
+  }
+
+  let guess = nn.predict(inputs);
+
+  let m = max(guess);
+
+  console.log(m);
+  let classification = guess.indexOf(m);
+
+  if (m < 0.5) {
+    classification = -1;
+  }
+  m = nf(m * 100, 2,0) + "% SURE A ";
+  if (classification === CAT) {
+    console.log("cat");
+    infotext.innerHTML = m + "CAT";
+  } else if (classification === RAINBOW) {
+    console.log("rainbow");
+    infotext.innerHTML = m + "RAINBOW";
+  } else if (classification === TRAIN) {
+    console.log("train");
+    infotext.innerHTML = m + "TRAIN ";
+  } else {
+    console.log("??");
+    infotext.innerHTML = "DON'T KNOW";
+  }
+}
+
+  //image(img, 0, 0)
--- examples/doodle_classification/traintest.js
+++ examples/doodle_classification/traintest.js
@@ -1,17 +1,40 @@
-function trainEpoch(training) {
-  shuffle(training, true);
+
+// function trainEpoch(training) {
+//   shuffle(training, true);
+//   //console.log(training);
+//   // Train for one epoch
+//   for (let i = 0; i < training.length; i++) {
+//     let data = training[i];
+//     let inputs = Array.from(data).map(x => x / 255);
+//     let label = training[i].label;
+//     let targets = [0, 0, 0];
+//     targets[label] = 1;
+//     // console.log(inputs);
+//     // console.log(targets);
+    
+//     nn.train(inputs, targets);
+//   }
+// }
+
+function trainEpoch(training, l) {
+  //shuffle(training, true);
   //console.log(training);
   // Train for one epoch
-  for (let i = 0; i < training.length; i++) {
-    let data = training[i];
+  for(i=0;i < 25;i++,l++) {
+    let data = training[l];
     let inputs = Array.from(data).map(x => x / 255);
-    let label = training[i].label;
+    let label = training[l].label;
     let targets = [0, 0, 0];
     targets[label] = 1;
     // console.log(inputs);
     // console.log(targets);
+    //console.log(l);
+    stroke(0,0,255);
+    rect(0,canvas.height-5,floor((canvas.width/training.length)*l) ,3);
+    //line(0,0,floor((canvas.width/training.length)*l) ,1);
     nn.train(inputs, targets);
   }
+  return l;
 }
 
 function testAll(testing) {
